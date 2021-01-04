82cc6cdbcd6b8d46baa2bfdb1a7ad72c2975afc7
gnikhil27 <gnikhil@email.com>
2021-01-04 18:18:12 +0530

    Added Combo

--- tech/adder.js
+++ tech/adder.js
@@ -1,3 +1,5 @@
+var GEN=["Gen3rd","Gen9th"];
+var HDD=["1TB","150GB"];
 function add()
 {
     var pr=document.getElementsByClassName("price");
@@ -9,11 +11,35 @@ function add()
     document.getElementById("txtTotal").innerHTML=tot;
     return tot;
 }
-function getBoard()
+function getAll()
 {
-    var gen={
-            Gen3rd: "",
-            Gen5th: "",
-            Gen7th: ""
-        }
+    var gen="";
+    GEN.forEach(function(val){gen+="<option value='"+val+"'>"+val+"</option>";})
+    var myBoards=document.getElementById("Gen").innerHTML=gen;
+    gen="";
+    HDD.forEach(function(val){gen+="<option value='"+val+"'>"+val+"</option>";})
+    myBoards=document.getElementById("hdd").innerHTML=gen;
+    //changeBoard(myBoards);
+    add();
+}
+function changeBoard(selBoard){
+    var mb={
+            Gen3rd: "3200",
+            Gen9th:"5500"
+        };
+    var proc={
+        Gen3rd: "2500",
+        Gen9th:"7200"
+    }
+    document.getElementById("mbPrice").innerHTML=mb[selBoard.value];
+    document.getElementById("ProcPrice").innerHTML=proc[selBoard.value];
+    add();
+}
+function changeHDD(selHDD){
+    var hd={
+        "1TB": "3000",
+        "150GB": "800"
+    };
+    document.getElementById("hddPrice").innerHTML=hd[selHDD.value];
+    add();
 }
