f12c69f89550994b1280ea2539e622ac911385d0
Nikhil Gupta <gnikhil@email.com>
2021-04-08 13:23:16 +0530

    Will Make some More Changes

---
+++ t3/android/mode.js
@@ -0,0 +1,7 @@
+function toggleMode(){
+    var mainBody = document.getElementById("mainBody");
+    mainBody.classList.toggle("darkMode");
+}
+function checkDarkMode(){
+    var i = document.cookie;
+}
